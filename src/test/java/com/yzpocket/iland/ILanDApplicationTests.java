53c129e5601069649b6e6a8769dff5caa538b200
INYONG KIM <ohnyong87@gmail.com>
2023-11-15 19:54:50 +0900

    chore : First init the iland project

---
+++ src/test/java/com/yzpocket/iland/ILanDApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yzpocket.iland;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ILanDApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
