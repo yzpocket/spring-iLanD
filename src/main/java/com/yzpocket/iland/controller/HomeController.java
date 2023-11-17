fed1ccbb6011a74c99719b7e26cfa774ebac6ff5
INYONG KIM <ohnyong87@gmail.com>
2023-11-17 17:07:10 +0900

    chore : 필수 의존성 주입 및 HomeController를 통한 타임리프엔진 뷰페이지 반환 테스트

---
+++ src/main/java/com/yzpocket/iland/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.yzpocket.iland.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class HomeController {
+    @GetMapping("/")
+    public String home(){
+        return "iLanD_main";
+    }
+}
