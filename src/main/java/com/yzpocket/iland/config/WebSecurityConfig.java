80e8157d3b9a883eab5f66d216853521f387ebb1
INYONG KIM <ohnyong87@gmail.com>
2023-11-19 23:40:03 +0900

    refactor : 로그인,로그아웃 주석 정리

--- src/main/java/com/yzpocket/iland/config/WebSecurityConfig.java
+++ src/main/java/com/yzpocket/iland/config/WebSecurityConfig.java
@@ -20,7 +20,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-@EnableWebSecurity // Spring Security 지원을 가능하게 함
+@EnableWebSecurity
 @RequiredArgsConstructor
 public class WebSecurityConfig {
 
--- src/main/java/com/yzpocket/iland/security/UserDetailsImpl.java
+++ src/main/java/com/yzpocket/iland/security/UserDetailsImpl.java
@@ -9,6 +9,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.ArrayList;
 import java.util.Collection;
 
+
 public class UserDetailsImpl implements UserDetails {
 
     private final User user;
